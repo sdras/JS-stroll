86003b3c3d3b5729ff9356eb1f9feebdc0d29257
Sarah Drasner <sarah.drasner@gmail.com>
2016-05-01 11:20:14 -0700

    add in part 4 with gravity, remove unnecessary render call

---
+++ 7-hack-physics-and-js-nocanvas-svg-4/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/sdras/pen/2bdd461ea2d56a6c1403c8a9282dc6fe.
+
+ 
--- 7-hack-physics-and-js-nocanvas-svg-4/js/index.js
+++ 7-hack-physics-and-js-nocanvas-svg-4/js/index.js
@@ -1,5 +1,6 @@
 // rewritten from Canvas to SVG and Vanilla JS from this post http://codepen.io/rachsmith/post/hack-physics-and-javascript-1
-// live demo at http://codepen.io/sdras/pen/4d353ad01bab58a19d32abf1d435fcaa
+// live demo at http://codepen.io/sdras/pen/2bdd461ea2d56a6c1403c8a9282dc6fe
+// rewrote particle and create particle function to reduce repetition, but this might have made it more costly because it holds more state.
 
 var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg"),
     svgNS = svg.namespaceURI,
@@ -10,7 +11,8 @@ var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg"),
     width = window.innerWidth,
     height = window.innerHeight,
     particles = [],
-    colors = ['#029DAF', '#E5D599', '#FFC219', '#F07C19', '#E32551'];
+    colors = ['#029DAF', '#E5D599', '#FFC219', '#F07C19', '#E32551'],
+    gravity = 0.04;
 
 document.body.appendChild(svg);
 document.body.style.background = '#222';
@@ -18,7 +20,6 @@ svg.setAttribute("viewBox", "0 0 " + width + " " + height);
 svg.setAttribute("width", width);
 svg.setAttribute("height", height);
 
-//pretty much unchanged from Rachels
 function initParticles() {
   for (var i = 0; i < 200; i++) {
     setTimeout(createParticle, 20*i, i);
@@ -26,28 +27,41 @@ function initParticles() {
 }
 
 function createParticle(i) {
-  // initial position in middle of viewbox
-  var x = width*0.5;
-  var y = height*0.5;
-  // randomize the acceleration a little - but we still want them flying 'up' and 'out'
-  var accelX = -2+Math.random()*4;
-  var accelY = Math.random()*-3;
-  // randomize size and opacity a little & pick a color from our color palette
-  var size = 5+Math.random()*5;
-  var color = colors[i%colors.length];
-  var opacity =  0.5 + Math.random()*0.5;
-  var p = new Particle(x, y, accelX, accelY, size, color, opacity);
+  var p = new Particle(i);
   particles.push(p);
 }
 
-//we create the particles differently because of SVG drawing, we don't need draw() but we still update.
-function Particle(x, y, accelX, accelY, size, color, opacity) {
+function Particle(i) {
+  // initial position in middle of viewbox
+  var x,
+      y,
+      accelX,
+      accelY,
+      opacity,
+      size = 5+Math.random()*5,
+      color = colors[i%colors.length];
+  
   var circ = document.createElementNS(svgNS, "circle");
   svg.appendChild(circ);
   circ.setAttribute("r", (size/2));
   circ.setAttribute("fill", color);
+      
+  this.reset = function() {
+    x = width*0.5;
+    y = height*0.5;
+ 
+    opacity = 0.5 + Math.random()*0.5;
+    accelX = -2+Math.random()*4;
+    accelY = Math.random()*-3;
+  };
+  
+  this.reset();
   
   this.update = function() {
+    if (opacity - 0.005 > 0) opacity -= 0.005 ;
+    else this.reset();
+    
+    accelY += gravity;
     x += accelX;
     y += accelY;
     
